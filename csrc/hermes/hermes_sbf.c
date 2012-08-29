685eaacd26bd9c21c69111fbcee799a6a3f28ed1
root <root@tareka01.cs.uiuc.edu>
2012-08-29 17:03:35 -0500

    started

---
+++ csrc/hermes/hermes_sbf.c
@@ -0,0 +1,14 @@
+#include<sbf.h>
+
+
+uint8_t sbf_create(uint32_t estimated_members, uint8_t bf_count){
+}   
+
+uint8_t sbf_insert(char * name, uint16_t len){
+}
+
+uint8_t sbf_check(char * name, uint16_t len){
+}
+
+void sbf_swap(){
+} 
---
+++ csrc/hermes/hermes_sbf.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
+#include<string.h>
+
+uint8_t sbf_insert(char *, uint16_t);
+uint8_t sbf_check(char *, uint16_t);
+void sbf_swap(void);
+
